8e7ac3385c074137d3388bfbf2822acd4d646d63
Wesley Mak <wesleycpmak@gmail.com>
2024-06-05 02:15:52 +0800

    initalized spring project

---
+++ server/src/test/java/com/tradingPlat/proj/ProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradingPlat.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
