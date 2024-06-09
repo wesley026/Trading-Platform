7dd0bc4e2b9c91b78c3582b2def50063ed87ab0c
Wesley Mak <wesleycpmak@gmail.com>
2024-06-09 19:15:25 +0800

    Added custom login and logout features

---
+++ server/src/main/java/com/tradingPlat/proj/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.tradingPlat.proj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String showHome() {
+
+        return "home";
+    }
+}
--- server/src/main/java/com/tradingPlat/proj/entity/TransactionType.java
+++
@@ -1,6 +0,0 @@
-package com.tradingPlat.proj.entity;
-
-public enum TransactionType {
-    BUY,
-    SELL
-}
